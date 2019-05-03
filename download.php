ef3bf859ba5c8d5872b57cc4e51c88aa4aa7da13
Simson Rinekso <simson@rienkso.id>
2019-05-03 14:27:17 +0700

    email fixing

--- download.php
+++ download.php
@@ -2,7 +2,7 @@
 session_start();
 $name=@$_SESSION['nameTictech'];
 $company = @$_SESSION['companyTictech'];
-if(@$_SESSION[$name."-".$company] == 0){
+if(@$_SESSION[$name."-".$company] == 1){
 	echo "you have been download this portofolio, please use other name to download in our website, <a href='http://tictechstudio.com'>TicTech Studio</a>.";
 }else{
 	$_SESSION[$name."-".$company] = 1;
