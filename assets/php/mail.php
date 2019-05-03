53cf1beb296f3fd4782a96ff4317e11369928cf6
Simson Rinekso <simson@rienkso.id>
2019-05-03 15:41:38 +0700

    captcha fixing

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -2,36 +2,47 @@
 use PHPMailer\PHPMailer\PHPMailer;
 require '../plugins/vendor/autoload.php';
 
-$mail = new PHPMailer; // create a new object
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
-// $mail->SMTPOptions = array(
-//    'ssl' => array(
-//      'verify_peer' => false,
-//      'verify_peer_name' => false,
-//      'allow_self_signed' => true
-//     )
-// );
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 587; // or 587
-$mail->IsHTML(true);
-$mail->Username = "lanius.agni@gmail.com";
-$mail->Password = "Onestepahead1212";
-$mail->SetFrom("no-reply@tictechstudio.com");
-$mail->Subject = "Anda mendapat message melalui website Tictechstudio.com";
-$mail->Body =   	'Nama : '.$_POST['name'].'<br>'.
-  	'Email : '.$_POST['email'].'<br>'.
-  	'Pesan : '.$_POST['comment'].'<br>';
-$mail->AddAddress("reyno33333@gmail.com");
-$mail->Send();
- if(!$mail->Send()) {
-  // echo $mail->Body;
-  //   echo "<br> Mailer Error: " . $mail->ErrorInfo;
- } else {
- }
-    header('location:../../.?feedback=1');
+$secretKey = '6Le2jqEUAAAAAGLL4CxwX8BTZ-_OlSn6SKfOl2x0';
+$captcha = $_POST['g-recaptcha-response'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+$responseKeys = json_decode($response,true);
+
+if(intval($responseKeys["success"]) !== 1) {
+  echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
+} else {
+
+  $mail = new PHPMailer; // create a new object
+  $mail->IsSMTP(); // enable SMTP
+  $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+  $mail->SMTPAuth = true; // authentication enabled
+  $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+  // $mail->SMTPOptions = array(
+  //    'ssl' => array(
+  //      'verify_peer' => false,
+  //      'verify_peer_name' => false,
+  //      'allow_self_signed' => true
+  //     )
+  // );
+  $mail->Host = "smtp.gmail.com";
+  $mail->Port = 587; // or 587
+  $mail->IsHTML(true);
+  $mail->Username = "lanius.agni@gmail.com";
+  $mail->Password = "Onestepahead1212";
+  $mail->SetFrom("no-reply@tictechstudio.com");
+  $mail->Subject = "Anda mendapat message melalui website Tictechstudio.com";
+  $mail->Body =   	'Nama : '.$_POST['name'].'<br>'.
+    	'Email : '.$_POST['email'].'<br>'.
+    	'Pesan : '.$_POST['comment'].'<br>';
+  $mail->AddAddress("reyno33333@gmail.com");
+  $mail->Send();
+   if(!$mail->Send()) {
+    // echo $mail->Body;
+    //   echo "<br> Mailer Error: " . $mail->ErrorInfo;
+   } else {
+   }
+      header('location:../../.?feedback=1');
+}
  /*
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
   ->setUsername('reyno33333@gmail.com')
--- assets/php/portofolio.php
+++ assets/php/portofolio.php
@@ -2,38 +2,48 @@
 use PHPMailer\PHPMailer\PHPMailer;
 require '../plugins/vendor/autoload.php';
 
-session_start();
-$_SESSION['name'] = $_POST['name'];
-$_SESSION['company'] = $_POST['company'];
-$_SESSION[$_POST['name']."-".$_POST['company']] = 0;
+$secretKey = '6Le2jqEUAAAAAGLL4CxwX8BTZ-_OlSn6SKfOl2x0';
+$captcha = $_POST['g-recaptcha-response'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+$responseKeys = json_decode($response,true);
 
-$mail = new PHPMailer; // create a new object
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
-// $mail->SMTPOptions = array(
-//    'ssl' => array(
-//      'verify_peer' => false,
-//      'verify_peer_name' => false,
-//      'allow_self_signed' => true
-//     )
-// );
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 587; // or 587
-$mail->IsHTML(true);
-$mail->Username = "lanius.agni@gmail.com";
-$mail->Password = "Onestepahead1212";
-$mail->SetFrom("no-reply@tictechstudio.com");
-$mail->Subject = "Portofolio of Tictech";
-$mail->Body =  'Greetings '.$_POST['name'].' from '.$_POST['company'].' ! Here is link to download our portfolio: <a href="http://tictechstudio.com/download.php">Download Portofolio</a>. Whoever you are, we hope to collaborate in creating a masterpiece project with you in the future. You can check our contact list in our portfolio, so, please feel free to consult with us. we look forward to hearing from you in the future!<br>
-<br>
-Fandi Alfiansah<br>
-Head of Production<br>
-TicTech Studio<br>';
-$mail->AddAddress($_POST['email']);
-$mail->Send();
-header('location:../../.?feedback=2');
+if(intval($responseKeys["success"]) !== 1) {
+  echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
+} else {
+  session_start();
+  $_SESSION['name'] = $_POST['name'];
+  $_SESSION['company'] = $_POST['company'];
+  $_SESSION[$_POST['name']."-".$_POST['company']] = 0;
+
+  $mail = new PHPMailer; // create a new object
+  $mail->IsSMTP(); // enable SMTP
+  $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+  $mail->SMTPAuth = true; // authentication enabled
+  $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+  // $mail->SMTPOptions = array(
+  //    'ssl' => array(
+  //      'verify_peer' => false,
+  //      'verify_peer_name' => false,
+  //      'allow_self_signed' => true
+  //     )
+  // );
+  $mail->Host = "smtp.gmail.com";
+  $mail->Port = 587; // or 587
+  $mail->IsHTML(true);
+  $mail->Username = "lanius.agni@gmail.com";
+  $mail->Password = "Onestepahead1212";
+  $mail->SetFrom("no-reply@tictechstudio.com");
+  $mail->Subject = "Portofolio of Tictech";
+  $mail->Body =  'Greetings '.$_POST['name'].' from '.$_POST['company'].' ! Here is link to download our portfolio: <a href="http://tictechstudio.com/download.php">Download Portofolio</a>. Whoever you are, we hope to collaborate in creating a masterpiece project with you in the future. You can check our contact list in our portfolio, so, please feel free to consult with us. we look forward to hearing from you in the future!<br>
+  <br>
+  Fandi Alfiansah<br>
+  Head of Production<br>
+  TicTech Studio<br>';
+  $mail->AddAddress($_POST['email']);
+  $mail->Send();
+  header('location:../../.?feedback=2');
+}
  /*
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
   ->setUsername('reyno33333@gmail.com')
