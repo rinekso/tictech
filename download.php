90f328590588bd44da3c0f0d409f0ea1a389d42f
Simson Rinekso <simson@rienkso.id>
2019-05-03 14:21:34 +0700

    email fixing

--- download.php
+++ download.php
@@ -2,9 +2,9 @@
 session_start();
 $name=$_SESSION['nameTictech'];
 $company = $_SESSION['companyTictech'];
-if(@$_SESSION[$name]."-".$company]] == 0){
+if(@$_SESSION[$name."-".$company] == 0){
 	echo "you have been download this portofolio, please use other name to download in our website, TicTech Studio."
 }else{
-	$_SESSION[$name]."-".$company]] = 1;
+	$_SESSION[$name."-".$company] = 1;
 	header("assets/pdf/TICTECH-PORTOFOLIO.pdf");
 }
