f8b9698d1f39195b0893269d1447e697f8196eb6
Simson Rinekso <simson@rienkso.id>
2019-05-03 15:30:33 +0700

    captcha fixing

--- index.php
+++ index.php
@@ -491,6 +491,47 @@
 <!-- <script src="assets/plugins/luxy.js-master/dist/js/luxy.min.js" charset="utf-8"></script> -->
 <script src="assets/plugins/slick-1.8.1/slick/slick.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" src="assets/js/custom.js"></script>
+<script type="text/javascript">
+(function ($) {
+    'use strict';
+
+    var form = $('form'),
+        // message = $('.msg'),
+        form_data;
+
+    // Success function
+    function done_func(response) {
+        // message.fadeIn()
+        // message.html(response);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 5000);
+        
+        form.find('input:not([type="submit"]), textarea').val('');
+    }
+
+    // fail function
+    function fail_func(data) {
+        // message.fadeIn()
+        // message.html(data.responseText);
+        setTimeout(function () {
+            message.fadeOut(5000);
+        }, 5000);
+    }
+    
+    form.submit(function (e) {
+        e.preventDefault();
+        form_data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: form_data
+        })
+        .done(done_func)
+        .fail(fail_func);
+    });
+})(jQuery);
+</script>
 <?php
 if(@$_GET['feedback'] == "1"){
 ?>
