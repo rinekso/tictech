b9fa5c7d4207580873c5d61cb7555caf176d98bb
Simson Rinekso <simson@rienkso.id>
2019-05-03 14:23:16 +0700

    email fixing

--- download.php
+++ download.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$name=$_SESSION['nameTictech'];
-$company = $_SESSION['companyTictech'];
+$name=@$_SESSION['nameTictech'];
+$company = @$_SESSION['companyTictech'];
 if(@$_SESSION[$name."-".$company] == 0){
-	echo "you have been download this portofolio, please use other name to download in our website, TicTech Studio."
+	echo "you have been download this portofolio, please use other name to download in our website, <a href='http://tictechstudio.com'>TicTech Studio</a>.";
 }else{
 	$_SESSION[$name."-".$company] = 1;
 	header("assets/pdf/TICTECH-PORTOFOLIO.pdf");
