db5ab57545e0cf293cb95816f204f2e630717197
Simson Rinekso <simson@rienkso.id>
2019-05-08 11:31:07 +0700

    fixing

--- download.php
+++ download.php
@@ -6,7 +6,7 @@ if(@$_SESSION[$name."-".$company] == 2){
 	echo "you have been download this portofolio, please use other name to download in our website, <a href='http://tictechstudio.com'>TicTech Studio</a>.";
 }else{
 	$_SESSION[$name."-".$company] = 1;
-	$filepath = "./assets/pdf/TICTECH-PORTOFOLIO.pdf";
+	$filepath = "./assets/pdf/TICTECH-PORTFOLIO.pdf";
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
