f36541c8f129b79658934801d72541dd04f6c99e
rinekso <reyno33333@gmail.com>
2020-01-06 15:41:13 +0700

    update download script

--- download.php
+++ download.php
@@ -6,7 +6,8 @@ if(@$_SESSION[$name."-".$company] == 2){
 	echo "you have been download this portofolio, please use other name to download in our website, <a href='http://tictechstudio.com'>TicTech Studio</a>.";
 }else{
 	$_SESSION[$name."-".$company] = 1;
-	$filepath = "./assets/pdf/TICTECH-PORTFOLIO.pdf";
+	$filepath = "./assets/pdf/TICTECH-PORTOFOLIO.pdf";
+
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
@@ -14,11 +15,24 @@ if(@$_SESSION[$name."-".$company] == 2){
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: ' . filesize($filepath));
+    // echo $filepath;
     flush(); // Flush system output buffer
     readfile($filepath);
     exit;
 }
+function retrieve_remote_file_size($url){
+     $ch = curl_init($url);
 
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+     curl_setopt($ch, CURLOPT_HEADER, TRUE);
+     curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+
+     $data = curl_exec($ch);
+     $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+     curl_close($ch);
+     return $size;
+}
 function addUser(){
     $nama = $path->nama();
     $val = $path->val();
