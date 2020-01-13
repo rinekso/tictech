d24b503915bba0d3915bc529c06958bb9ecbf023
simsonrinekso <reyno33333@gmail.com>
2020-01-13 11:48:40 +0700

    test

---
+++ photo/upload.php
@@ -0,0 +1,22 @@
+<?php
+echo $_POST['name'];
+echo $_FILES['file']['name'];
+/*
+if(!empty(@$_FILES['file']['name'])){
+	$file = $_FILES['file'];
+	$name = $file['name'];
+	$path_parts = pathinfo($name);
+	$ext = $path_parts['extension'];
+	$rand = rand(10000,99999);
+	$name = $rand.".".$ext;
+	$r = move_uploaded_file($_FILES['file']['tmp_name'], 'storage/'.$name);
+	if($r)
+		echo $_SERVER['SERVER_NAME']."/photo/storage/".$name;
+	else
+		echo "nope, somethings wrong";
+}else{
+	// phpinfo();
+	echo "there is no file to upload";
+}
+*/
+?>
