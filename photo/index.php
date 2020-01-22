11882ab822449b71d60575a7a864f8614c1c129c
simsonrinekso <reyno33333@gmail.com>
2020-01-22 22:27:53 +0700

    update index.php

---
+++ photo/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+   <meta charset="UTF-8">
+   <link rel="shortcut icon" href="./storage/favicon.ico">
+   <title>Directory Contents</title>
+
+   <link rel="stylesheet" href="./storage/style.css">
+   <script src="./storage/sorttable.js"></script>
+</head>
+
+<body>
+<div id="container">
+	<h1>Sorry, you must not access this url</h1>
+</div>
+</body>
+</html>
--- photo/storage/index.php
+++ photo/storage/index.php
@@ -2,11 +2,11 @@
 <html>
 <head>
    <meta charset="UTF-8">
-   <link rel="shortcut icon" href="./.favicon.ico">
+   <link rel="shortcut icon" href="./favicon.ico">
    <title>Directory Contents</title>
 
-   <link rel="stylesheet" href="./.style.css">
-   <script src="./.sorttable.js"></script>
+   <link rel="stylesheet" href="./style.css">
+   <script src="./sorttable.js"></script>
 </head>
 
 <body>
@@ -134,6 +134,8 @@
 		}
 
 	// Output
+	$extn=pathinfo($dirArray[$index], PATHINFO_EXTENSION);
+	if($extn == "jpg")
 	 echo("
 	 	<a class='item' target='_blank' href='./$namehref'$favicon><img src='./$namehref'/></a>
 ");
