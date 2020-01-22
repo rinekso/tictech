da114af963c7a2ae6eaffd024c759983ca0bed74
simsonrinekso <reyno33333@gmail.com>
2020-01-22 22:01:50 +0700

    update photo upload php

--- photo/upload.php
+++ photo/upload.php
@@ -6,9 +6,31 @@ if(!empty(@$_FILES['file']['name'])){
 	$ext = $path_parts['extension'];
 	$rand = rand(10000,99999);
 	$name = $rand.".".$ext;
+
+	// Opens directory
+	$myDirectory=opendir("storage/");
+
+	// Gets each entry
+	while($entryName=readdir($myDirectory)) {
+	   $dirArray[]=$entryName;
+	}
+
+	// Closes directory
+	closedir($myDirectory);
+
+	// Counts elements in array
+	$indexCount=count($dirArray);
+
+	// Sorts files
+	sort($dirArray);
+
+	if($indexCount == 0)
+		$name = "NyayataXTictechPhotobooth-0.jpg";
+	else
+		$name = "NyayataXTictechPhotobooth-".($indexCount-7).".jpg";
 	$r = move_uploaded_file($_FILES['file']['tmp_name'], 'storage/'.$name);
 	if($r)
-		echo "https://".$_SERVER['SERVER_NAME']."/photo/storage/".$name;
+		echo "https://".$_SERVER['SERVER_NAME']."/photo/storage/";
 	else
 		echo "nope, somethings wrong";
 }else{
