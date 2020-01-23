ae4b336da4ef066d34d895b1f3a754cb08ca4c3a
simsonrinekso <reyno33333@gmail.com>
2020-01-23 14:18:08 +0700

    add storage2

---
+++ photo/upload2.php
@@ -0,0 +1,41 @@
+<?php
+if(!empty(@$_FILES['file']['name'])){
+	$file = $_FILES['file'];
+	$name = $file['name'];
+	$path_parts = pathinfo($name);
+	$ext = $path_parts['extension'];
+	$rand = rand(10000,99999);
+	$name = $rand.".".$ext;
+
+	// Opens directory
+	$myDirectory=opendir("storage/");
+
+	// Gets each entry
+	while($entryName=readdir($myDirectory)) {
+	   $dirArray[]=$entryName;
+	}
+
+	// Closes directory
+	closedir($myDirectory);
+
+	// Counts elements in array
+	$indexCount=count($dirArray);
+
+	// Sorts files
+	sort($dirArray);
+
+	if($indexCount == 0)
+		$name = "NyayataXTictechPhotobooth-0.jpg";
+	else
+		$name = "NyayataXTictechPhotobooth-".($indexCount-7).".jpg";
+	$r = move_uploaded_file($_FILES['file']['tmp_name'], 'storage/'.$name);
+	if($r)
+		echo "https://".$_SERVER['SERVER_NAME']."/photo/storage2/";
+	else
+		echo "nope, somethings wrong";
+}else{
+	// phpinfo();
+	echo "there is no file to upload";
+}
+
+?>
