ca1d69f9ee8010447e5d7a6e0fb7572a45404590
simsonrinekso <reyno33333@gmail.com>
2020-03-27 13:01:04 +0700

    update captcha

--- assets/php/comment.php
+++ assets/php/comment.php
@@ -3,45 +3,45 @@ use PHPMailer\PHPMailer\PHPMailer;
 require '../plugins/vendor/autoload.php';
 
 $secretKey = '6LcZkOMUAAAAAJtOQnfvx25iDZu2VllitUQ4OFwf';
-require_once('../plugins/recaptcha-php-1.11/recaptchalib.php');
-  $privatekey = $secretKey;
-  $resp = recaptcha_check_answer ($privatekey,
-                                $_SERVER["REMOTE_ADDR"],
-                                $_POST["recaptcha_challenge_field"],
-                                $_POST["recaptcha_response_field"]);
-
-  if (!$resp->is_valid) {
-    // What happens when the CAPTCHA was entered incorrectly
-    die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
-         "(reCAPTCHA said: " . $resp->error . ")");
-  }else{
-  $mail = new PHPMailer; // create a new object
-  $mail->IsSMTP(); // enable SMTP
-  $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-  $mail->SMTPAuth = true; // authentication enabled
-  $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
-  // $mail->SMTPOptions = array(
-  //    'ssl' => array(
-  //      'verify_peer' => false,
-  //      'verify_peer_name' => false,
-  //      'allow_self_signed' => true
-  //     )
-  // );
-  $mail->Host = "smtp.gmail.com";
-  $mail->Port = 587; // or 587
-  $mail->IsHTML(true);
-  $mail->Username = "info@tictechstudio.com";
-  $mail->Password = "Sebatdulu1";
-  $mail->SetFrom("no-reply@tictechstudio.com");
-  $mail->Subject = "you had a message";
-  $mail->Body =
-      'Nama : '.$_POST['name'].'<br>'.
-      'Email : '.$_POST['email'].'<br>'.
-      'Pesan : '.$_POST['comment'].'<br>';
-  $mail->AddAddress("agammail95@gmail.com");
-  $mail->Send();
-  header('location:../../.?feedback=2');
-}
+  if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))
+  {
+        $secret = 'your_actual_secret_key';
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+        if($responseData->success)
+        {
+          $mail = new PHPMailer; // create a new object
+          $mail->IsSMTP(); // enable SMTP
+          $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+          $mail->SMTPAuth = true; // authentication enabled
+          $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+          // $mail->SMTPOptions = array(
+          //    'ssl' => array(
+          //      'verify_peer' => false,
+          //      'verify_peer_name' => false,
+          //      'allow_self_signed' => true
+          //     )
+          // );
+          $mail->Host = "smtp.gmail.com";
+          $mail->Port = 587; // or 587
+          $mail->IsHTML(true);
+          $mail->Username = "info@tictechstudio.com";
+          $mail->Password = "Sebatdulu1";
+          $mail->SetFrom("no-reply@tictechstudio.com");
+          $mail->Subject = "you had a message";
+          $mail->Body =
+              'Nama : '.$_POST['name'].'<br>'.
+              'Email : '.$_POST['email'].'<br>'.
+              'Pesan : '.$_POST['comment'].'<br>';
+          $mail->AddAddress("agammail95@gmail.com");
+          $mail->Send();
+          header('location:../../.?feedback=2');
+        }
+        else
+        {
+          header('location:../../.?feedback=1');
+        }
+   }
 // }
  /*
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
